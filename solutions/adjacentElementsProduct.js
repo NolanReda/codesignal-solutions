7b4797c66e594e8bdf1db1b9de682ce309dee0eb
Nolan Reda <nolan.reda@gmail.com>
2022-06-28 01:00:08 +0000

    adjacent

---
+++ solutions/adjacentElementsProduct.js
@@ -0,0 +1,15 @@
+function solution(inputArray) {
+  var first = 0;
+  var second = 0;
+  var collection = [];
+  for (let i = 0; i < inputArray.length; i++) {
+    first = inputArray[i];
+    second = inputArray[i + 1];
+    if (i === inputArray.length - 1) {
+      continue;
+    }
+    var sum = first * second;
+    collection.push(sum);
+  }
+  return Math.max(...collection);
+}
