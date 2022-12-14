1b4ce707b902317897ee586e8fcc1ddb1e769798
Nolan Reda <nolan.reda@gmail.com>
2022-12-14 05:09:39 +0000

    longest common prefix

--- solutions/longestCommonPrefix.js
+++ solutions/longestCommonPrefix.js
@@ -3,10 +3,9 @@ var longestCommonPrefix = function (strs) {
   if (strs === null || strs.length === 0) return pre;
 
   for (let i = 0; i < strs[0].length; i++) {
-    const char = strs[0][i]; // loop through all characters of the very first string.
+    const char = strs[0][i];
 
     for (let j = 1; j < strs.length; j++) {
-      // loop through all other strings in the array
       if (strs[j][i] !== char) return pre;
     }
     pre = pre + char;
